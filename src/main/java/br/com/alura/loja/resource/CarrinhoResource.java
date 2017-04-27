19a679ff5dd4679932fc58d3d3d28dd49f0979cf
Daniel T. Santos <dtsantos@padtec.com.br>
2017-04-27 15:51:05 -0300

    Aula 11 - método put
    Change-Id: I1a9a8e796833cea89bfb6652c40ca7054f231b4c

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,6 +17,7 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
@@ -41,6 +43,26 @@ public class CarrinhoResource {
 	public Response removeProduto(@PathParam("id") long id, @PathParam("id_produto") long idProduto) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(idProduto);
+		return Response.ok().build();
+	}
+
+	@Path("{id}/produtos/{id_produto}")
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response alteraProduto(@PathParam("id") long id, @PathParam("id_produto") long idProduto, String payload) {
+		Produto produto = (Produto) new XStream().fromXML(payload);
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		carrinho.troca(produto);
+		return Response.ok().build();
+	}
+
+	@Path("{id}/produtos/{id_produto}/quantidade")
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response alteraQuantidadeProduto(@PathParam("id") long id, @PathParam("id_produto") long idProduto, String payload) {
+		Produto produto = (Produto) new XStream().fromXML(payload);
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		carrinho.trocaQuantidade(produto);
 		return Response.ok().build();
 	}
 }
